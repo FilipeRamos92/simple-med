cde587ca576b6e37e0be73bb05ac69687f1f1bd5
Filipe <filipe.ramos92@hotmail.com>
2023-04-10 14:56:44 -0300

    Adding more test cases for speciality repository

--- src/test/java/com/system/simplemed/repository/SpecialityRepositoryTests.java
+++ src/test/java/com/system/simplemed/repository/SpecialityRepositoryTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import com.system.simplemed.model.Speciality;
 
 @DataJpaTest
@@ -23,6 +25,62 @@ public class SpecialityRepositoryTests {
         Speciality savedSpeciality = specialityRepository.save(speciality);
 
         assertThat(savedSpeciality).isNotNull();
-        assertThat(specialityRepository.count()).isGreaterThan(0);
+        assertThat(savedSpeciality.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void SpecialityRepository_FindAll_ReturnMoreThanOneSpeciality() {
+        Speciality speciality = Speciality.builder()
+            .name("Cardiologia").build();
+
+        Speciality speciality2 = Speciality.builder()
+            .name("Pediatria").build();
+
+        specialityRepository.save(speciality);
+        specialityRepository.save(speciality2);
+
+        List<Speciality> specialityList = specialityRepository.findAll();
+
+        assertThat(specialityList).isNotNull();
+        assertThat(specialityList.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void SpecialityRepository_FindById_ReturnSpeciality() {
+        Speciality speciality = Speciality.builder()
+            .name("Cardiologia").build();
+
+        specialityRepository.save(speciality);
+        
+        Speciality specialityById = specialityRepository.findById(speciality.getId()).get();
+
+        assertThat(specialityById).isNotNull();
+    }
+
+    @Test
+    public void SpecialityRepository_FindByName_ReturnSpecialityNotNull() {
+        Speciality speciality = Speciality.builder()
+            .name("Cardiologia").build();
+
+        specialityRepository.save(speciality);
+        Speciality specialitySaved = specialityRepository.findByName(speciality.getName()).get();
+
+        assertThat(specialitySaved).isNotNull();
+    }
+
+    @Test
+    public void SpecialityRepository_UpdateSpeciality_ReturnSpecialityNotNull() {
+        Speciality speciality = Speciality.builder()
+            .name("Cardiologia").build();
+
+        specialityRepository.save(speciality);
+        speciality.setName("Pediatria");
+
+        specialityRepository.save(speciality);
+
+        Speciality specialityUpdated = specialityRepository.findById(speciality.getId()).get();
+
+        assertThat(specialityUpdated).isNotNull();
+        assertThat(specialityUpdated.getName()).isEqualTo("Pediatria");
     }
 }
